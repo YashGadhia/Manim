3dd0f3f16325302c7b392fa28d06f5ff2aa05009
YashGadhia <59220404+YashGadhia@users.noreply.github.com>
2020-10-29 14:44:51 +0530

    Add files via upload

---
+++ manim_test_pulse_2.py
@@ -0,0 +1,50 @@
+from manimlib.imports import *
+
+class GraphFromData(GraphScene):
+    # Covert the data coords to the graph points
+    def get_points_from_coords(self,coords):
+        return [
+            # Convert COORDS -> POINTS
+            self.coords_to_point(px,py)
+            # See manimlib/scene/graph_scene.py
+            for px,py in coords
+        ]
+
+    # Return the dots of a set of points
+    def get_dots_from_coords(self,coords,radius=0.1):
+        points = self.get_points_from_coords(coords)
+        dots = VGroup(*[
+            Dot(radius=radius).move_to([px,py,pz])
+            for px,py,pz in points
+            ]
+        )
+        return dots
+
+class DiscreteGraphFromSetPoints(VMobject):
+    def __init__(self,set_of_points,**kwargs):
+        super().__init__(**kwargs)
+        self.set_points_as_corners(set_of_points)
+
+class CustomGraph3(GraphFromData):
+    CONFIG = {
+        "x_min": -2,
+        "x_max": 8,
+        "y_min": -1,
+        "y_max": 2,
+        "function_color": WHITE,
+        "axes_color": BLUE
+    }
+    def construct(self):
+        self.setup_axes()
+        x = [-2 , 3, 3, 5,  5,  8]
+        y = [1 , 1, 0, 0, 0.3, 0.3]
+
+        coords = [[px,py] for px,py in zip(x,y)]
+        # |
+        # V
+        points = self.get_points_from_coords(coords)
+        
+        graph = DiscreteGraphFromSetPoints(points,color=GREEN)
+        dots = self.get_dots_from_coords(coords)
+
+        self.add(graph,dots)
